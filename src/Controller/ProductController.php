34b93f224428d18e69697d0e176e96d810bcb7ef
Robin Payet <robinpayet@gmail.com>
2023-03-14 10:09:29 +0100

    Demo Echange BDD

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/product/create', name: 'app_product_create')]
+    public function create(EntityManagerInterface $entityManager): Response
+    {
+        $product = new Product();
+        $product->setName('Chaise');
+        $product->setprice(5999);
+        $product->setDescription('Un truc pour s\'asseoir...');
+        
+        $entityManager->persist($product); // Mets l'objet en attente
+        $entityManager->flush(); // INSERT INTO product
+
+        return $this->render('app_product', [
+            'controller_name' => 'ProductController',
+        ]);
+    }
+
+    #[Route('/product', name: 'app_product')]
+    public function index(ProductRepository $repository) 
+    {
+
+        $products = $repository->findAll(); // Donne un tableau avec tous les produits de la BDD
+
+        return $this->render('product/index.html.twig', [
+            'products' => $products,
+        ]);
+    }
+
+    #[Route('/product/{id}', name: 'app_product_show')]
+    public function show($id, ProductRepository $repository)
+    {
+        $product = $repository->find($id);
+
+        // 404 si le produit n'existe pas
+        if(!$product) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
