7e577584e11985bd6bcc8db5b4956a4fcc0edd6f
Robin Payet <robinpayet@gmail.com>
2023-03-15 11:11:00 +0100

    Faker Demo + Exercice Faker

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Post;
+use App\Entity\Product;
+use DateTimeImmutable;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Faker\Provider\Fakecar;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+
+        $faker = Factory::create('fr_FR'); // Méthode static
+        $faker->addProvider(new Fakecar($faker));
+
+        for ($i = 0; $i < 20; $i++) {
+            $product = new Product();
+            $product->setName($faker->vehicle());
+            $product->setDescription($faker->text());
+            $product->setPrice($faker->numberBetween(100, 2000) * 100);
+            $product->setImage('https://picsum.photos/id/'.rand(0, 1000).'/200/200');
+            $manager->persist($product);
+        }
+
+        for ($i = 0; $i < 100; $i++) {
+            $post = new Post();
+            $post->setName($faker->name());
+            $post->setContent($faker->text());
+            $post->setPublishedAt(DateTimeImmutable::createFromMutable($faker->datetime()));
+            $post->setActive((bool)random_int(0, 1));
+
+            $manager->persist($post);
+        }
+
+        $manager->flush();
+    }
+}
